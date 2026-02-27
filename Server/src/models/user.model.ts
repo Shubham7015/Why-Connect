308855d9a64ff946fc24c12a3a63cd804b5eb9c0
Shubham Rohilla <srohilla099@gmail.com>
2026-02-27 17:17:01 +0530

    update user model

--- Server/src/models/user.model.ts
+++ Server/src/models/user.model.ts
@@ -1,59 +1,70 @@
-import mongoose  , {Document , Schema }from "mongoose";
-import { hashValue, compareValue} from "../utils/bcrypt";
+import mongoose, { Document, Schema } from "mongoose";
+import { hashValue, compareValue } from "../utils/bcrypt";
 
 export interface UserDocument extends Document {
-    name: string;
-    email: string;
-    password: string;
-    avatar?: string | null;
-    createdAt: Date;
-    updatedAt: Date;
-    comparePassword: (value: string) => Promise<boolean>;
+  name: string;
+  email?: string;
+  password?: string;
+  avatar?: string | null;
+  isAI: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+
+  comparePassword: (value: string) => Promise<boolean>;
 }
 
-
-
-export const UserSchema = new Schema<UserDocument>({
-    name:{
-        type:String,
+export const UserSchema = new Schema<UserDocument>(
+  {
+    name: {
+      type: String,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        trim:true,
-        lowercase:true,
+    email: {
+      type: String,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      required: function (this: UserDocument) {
+        return !this.isAI;
+      },
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+      type: String,
+      required: function (this: UserDocument) {
+        return !this.isAI;
+      },
     },
-    avatar:{
-        type:String,
-        default:null,
-    }
-},{timestamps:true ,
-     toJSON: {
-    transform: (doc , ret) =>{
-        if(ret) {
-            delete (ret as any).password ;
+    avatar: {
+      type: String,
+      default: null,
+    },
+    isAI: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: (doc, ret) => {
+        if (ret) {
+          delete (ret as any).password;
         }
-        return ret ;
-    }
-},
-}
-)
+        return ret;
+      },
+    },
+  },
+);
 
 UserSchema.pre("save", async function () {
-    if (this.isModified("password") && this.password) {
-        this.password = await hashValue(this.password);
-    }
+  if (this.isModified("password") && this.password) {
+    this.password = await hashValue(this.password);
+  }
 });
 
-UserSchema.methods.comparePassword = async function(value: string){
-    return compareValue(value , this.password) ;
-}
+UserSchema.methods.comparePassword = async function (value: string) {
+  return compareValue(value, this.password);
+};
 
-const UserModel = mongoose.model<UserDocument>("User" , UserSchema) ;
+const UserModel = mongoose.model<UserDocument>("User", UserSchema);
 
-export default UserModel ;
+export default UserModel;
