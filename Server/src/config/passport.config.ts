5b6bcf9c9a75e836260f9c5ede1f6404b51c0922
Shubham Rohilla <srohilla099@gmail.com>
2026-02-28 21:45:02 +0530

    update authentication service

--- Server/src/config/passport.config.ts
+++ Server/src/config/passport.config.ts
@@ -1,32 +1,34 @@
 import passport from "passport";
-import {Strategy as JwtStrategy , ExtractJwt} from "passport-jwt";
+import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
 import { findIdUserService } from "../services/user.service";
 import { Env } from "../config/env.config";
 import { UnauthorizedException } from "../utils/app-error";
 
-
 passport.use(
-    new JwtStrategy(
-        {
-    jwtFromRequest: ExtractJwt.fromExtractors([
-        (req)=> {
-            const token = req.cookies.accessToken ;
-            if(!token) throw new UnauthorizedException("Unauthorized access") ;
-            return token ;
-        }
-    ]),
-    secretOrKey: Env.JWT_SECRET,
-    audience: ["user"],
-    algorithms: ["HS256"],
-   
-}, 
-async ({userId}, done) =>{
-    try {
-        const user = userId && (await findIdUserService(userId)) ;
-        return done(null , user || false) ;
-    }catch(error){
-        return done(null , false) ;
-    }
-}));
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req) => {
+          const token = req.cookies.accessToken;
+          return token || null;
+        },
+      ]),
+      secretOrKey: Env.JWT_SECRET,
+      audience: ["user"],
+      algorithms: ["HS256"],
+    },
+    async ({ userId }, done) => {
+      try {
+        const user = userId && (await findIdUserService(userId));
+        return done(null, user || false);
+      } catch (error) {
+        throw new UnauthorizedException("unauthorized to access");
+        return done(null, false);
+      }
+    },
+  ),
+);
 
-export const passportAuthenticateJwt = passport.authenticate("jwt" , {session:false})
+export const passportAuthenticateJwt = passport.authenticate("jwt", {
+  session: false,
+});
