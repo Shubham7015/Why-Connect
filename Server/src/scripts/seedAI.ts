7818a5f28d530fa0b6b46a7fe380ed99126f745d
Shubham Rohilla <srohilla099@gmail.com>
2026-02-28 21:47:48 +0530

    update ai model

--- Server/src/scripts/seedAI.ts
+++ Server/src/scripts/seedAI.ts
@@ -1,35 +1,33 @@
-import "dotenv/config"
-import UserModel from "../models/user.model"
-import connectDatabase from "../config/database.config"
+import "dotenv/config";
+import UserModel from "../models/user.model";
+import connectDatabase from "../config/database.config";
 
+export const CreateSeedAI = async () => {
+  const existingAI = await UserModel.findOne({ isAI: true });
+  if (existingAI) {
+    await UserModel.deleteOne({ _id: existingAI._id });
+  }
 
-export const CreateSeedAI = async()=>{
-    let seedAI = await UserModel.findOne({isAI: true}) ;
-    if(seedAI){
-        console.log("seed AI already exits");
-        return seedAI ;
-    }
-    seedAI =await UserModel.create({
-        name:"seedAI",
-        isAI:true,
-        avatar:
-        "https://res.cloudinary.com/ddfwqifrn/image/upload/v1771863623/ai-logo_iipcs2.png",
-    });
-    console.log("SeedAI created:" ,seedAI._id);
-    return seedAI ;
+  const seedAI = await UserModel.create({
+    name: "seedAI",
+    isAI: true,
+    avatar:
+      "https://res.cloudinary.com/ddfwqifrn/image/upload/v1771863623/ai-logo_iipcs2.png",
+  });
+  console.log("SeedAI created:", seedAI._id);
+  return seedAI;
 };
-
 
 const AI = async () => {
-    try{
-        await connectDatabase();
-        await CreateSeedAI();
-        console.log("Seeding completed") ;
-        process.exit(0) ;
-    }catch(err){
-        console.log("Seeding failed",err);
-        process.exit(1) ;
-    }
+  try {
+    await connectDatabase();
+    await CreateSeedAI();
+    console.log("Seeding completed");
+    process.exit(0);
+  } catch (err) {
+    console.log("Seeding failed", err);
+    process.exit(1);
+  }
 };
 
-AI() ;
+AI();
