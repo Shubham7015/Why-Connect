23366358c53018537915d8ccea3028f78590ceee
Shubham Rohilla <srohilla099@gmail.com>
2026-03-02 21:57:46 +0530

    update vite configuration

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -11,8 +11,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    host: true,
-    allowedHosts: ["melodi-unsaved-overcredulously.ngrok-free.dev"],
-  },
 });
